482940620291725c86bfe59b8fb3cf3d53182bdf
valeriazhil <valeria.zhil@gmail.com>
2020-05-09 08:05:42 +0300

    Tests Booking

--- day11/src/test/java/test1.java
+++ day11/src/test/java/test1.java
@@ -9,6 +9,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class test1 {
+
     //static WebDriver driver= new ChromeDriver();
 
     public static void main(String[] args) throws InterruptedException {
